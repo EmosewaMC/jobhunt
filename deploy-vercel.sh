ea83c4df28d77d5297817f08f800e67818f2434e
David Markowitz <EmosewaMC@gmail.com>
2024-01-20 20:46:56 -0800

    ci test

--- deploy-vercel.sh
+++ deploy-vercel.sh
@@ -4,7 +4,7 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
 # Install pagic
-/vercel/.deno/bin/deno install --unstable --allow-read --allow-write --allow-net https://deno.land/x/pagic/mod.ts
+/cmpm171-game/.deno/bin/deno install --unstable --allow-read --allow-write --allow-net https://deno.land/x/pagic/mod.ts
 
 # Pagic build
-/vercel/.deno/bin/deno run --unstable --allow-read --allow-write --allow-net --allow-env --allow-run https://deno.land/x/pagic/mod.ts build
+/cmpm171-game/.deno/bin/deno run --unstable --allow-read --allow-write --allow-net --allow-env --allow-run https://deno.land/x/pagic/mod.ts build
