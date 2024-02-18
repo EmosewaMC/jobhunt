29ef07f2f6e7ee84b7c4458d2d379a43b380535f
anranlee99 <anranlee99@gmail.com>
2024-02-18 14:57:57 -0800

    See if this fixes build

--- utils/db.ts
+++ utils/db.ts
@@ -1,14 +1,14 @@
-export const kv = await Deno.openKv();
+export const deno_kv = await Deno.openKv();
 
-const prefs = {
-  username: "ada",
-  theme: "dark",
-  language: "en-US",
-};
+// const prefs = {
+//   username: "ada",
+//   theme: "dark",
+//   language: "en-US",
+// };
 
-const result = await kv.set(["preferences", "ada"], prefs);
+// const result = await kv.set(["preferences", "ada"], prefs);
 
-const entry = await kv.get(["preferences", "ada"]);
-console.log(entry.key);
-console.log(entry.value);
-console.log(entry.versionstamp);
+// const entry = await kv.get(["preferences", "ada"]);
+// console.log(entry.key);
+// console.log(entry.value);
+// console.log(entry.versionstamp);
