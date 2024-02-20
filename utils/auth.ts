71c38722d48fd4e422aeb3b73eb15708be429d07
anranlee99 <anranlee99@gmail.com>
2024-02-20 13:51:17 -0800

    changes redir uri

--- utils/auth.ts
+++ utils/auth.ts
@@ -3,13 +3,14 @@ import { noRunOnBuild } from "./build.ts";
 
 // This checks if we are running on Deno Deploy (in production)
 const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
-
+const local_uri = Deno.env.get("LOCAL_REDIR_URI") as string;
+const deploy_uri = Deno.env.get("PROD_REDIR_URI") as string;
 // This is the OAuth configuration for Google Sign-In
 export const oAuthConfig = noRunOnBuild(() =>
   createGoogleOAuthConfig({
     redirectUri: isDenoDeploy
-      ? "https://hifi-gameplay-12.deno.dev/api/auth/callback"
-      : "http://localhost:8000/api/auth/callback",
+      ? deploy_uri
+      : local_uri,
     scope: [
       "https://www.googleapis.com/auth/userinfo.email",
       "https://www.googleapis.com/auth/userinfo.profile",
