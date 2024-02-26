b5ee021ddf480272b63b296a6998c630eac4abb7
David Markowitz <EmosewaMC@gmail.com>
2024-02-25 17:18:12 -0800

    lock down other pages from logged out users

--- routes/network.tsx
+++ routes/network.tsx
@@ -1,15 +1,26 @@
-import { useSignal } from "@preact/signals";
-import type { Signal } from "@preact/signals";
 import { NetworkButton, NetworkProps, NetworkList} from "../islands/NetworkButton.tsx";
-
-import { Handlers } from "$fresh/server.ts";
+import { getUser } from "$utils/get_user.ts";
+import type { Player } from "gameData/playerStats.ts";
 
 // import Form from "../islands/Form.tsx";
 
 // export default P5Canvas;
 
 //NOTE: This route will not be available through the nav later once we setup reaching here from the map route
-export default function Home() {
+export default async function Home(req: Request) {
+  const user = await getUser(req) as Player;
+  if (!user) {
+	return (
+	  <div class="px-4 py-8 mx-auto bg-[#86efac]">
+		<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+		  <h1 class="text-4xl font-bold">Welcome to the Jobhunt</h1>
+		  <a class="my-4 text-blue-500 underline" href="/api/auth/login">
+			Sign in to get started
+		  </a>
+		</div>
+	  </div>
+	);
+  }
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
--- routes/worldMap.tsx
+++ routes/worldMap.tsx
@@ -1,10 +1,23 @@
 import { useSignal } from "@preact/signals";
 import Map from "../islands/MapIsland.tsx";
+import { getUser } from "$utils/get_user.ts";
+import type { Player } from "gameData/playerStats.ts";
 
-export default function Home() {
+export default async function Home(req : Request) {
     //need to make the world map interactable
-
-  const count = useSignal(3);
+  const user = await getUser(req) as Player;
+  if (!user) {
+	return (
+	  <div class="px-4 py-8 mx-auto bg-[#86efac]">
+		<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+		  <h1 class="text-4xl font-bold">Welcome to the Jobhunt</h1>
+		  <a class="my-4 text-blue-500 underline" href="/api/auth/login">
+			Sign in to get started
+		  </a>
+		</div>
+	  </div>
+	);
+  }
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
