d0c5813dfc2e2cced59013142c599a8c75febfb0
David Markowitz <EmosewaMC@gmail.com>
2024-03-04 21:33:32 -0800

    Update Nav.tsx

--- islands/Nav.tsx
+++ islands/Nav.tsx
@@ -1,12 +1,17 @@
 import GoogleIcon from "icons/brand-google-filled.tsx";
 import { FreshContext } from "$fresh/server.ts";
+import { translate } from "gameData/locale.ts";
 
-export default function Nav() {
+export interface NavCtx {
+	  req: Request;
+};
+
+export default function Nav(ctx: NavCtx) {
   return (
     <>
       <aside class="w-64 h-full bg-gray-800 text-white flex flex-col">
         <div class="text-3xl text-center py-4 border-b border-gray-700">
-          <a href="/" class="py-2 hover:bg-gray-700 rounded">Logo</a>
+          <a href="/" class="py-2 hover:bg-gray-700 rounded">{translate("LOGO", ctx.req)}</a>
         </div>
         <nav class="flex flex-col flex-1 px-4 py-8">
           <a href="/" class="py-2 hover:bg-gray-700 rounded">Home</a>
