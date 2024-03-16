c574ca9b740cb04ca121e1b342003db15cc00f15
anranlee99 <anranlee99@gmail.com>
2024-03-15 18:27:19 -0700

    adds notification when player has unspent points

--- islands/NavLoggedIn.tsx
+++ islands/NavLoggedIn.tsx
@@ -1,8 +1,10 @@
 import GoogleIcon from "icons/brand-google-filled.tsx";
 import { translate } from "gameData/locale.ts";
-
+import type { Player } from "gameData/playerStats.ts";
+import { playerGainExperience } from "$utils/db.ts";
 export interface LoggedInCtx {
   req: Request;
+  user: Player;
 }
 
 export default function Nav(ctx: LoggedInCtx) {
@@ -10,15 +12,39 @@ export default function Nav(ctx: LoggedInCtx) {
     <>
       <aside class="w-64 h-screen bg-gray-800 text-white flex flex-col">
         <div class="text-3xl text-center py-4 border-b border-gray-700">
-          <a href="/" class="py-2 hover:bg-gray-700 rounded">{translate("LOGO", ctx.req)}</a>
+          <a href="/" class="py-2 hover:bg-gray-700 rounded">
+            {translate("LOGO", ctx.req)}
+          </a>
         </div>
         <nav class="flex flex-col flex-1 px-4 py-8">
-          <a href="/" class="py-2 hover:bg-gray-700 rounded">{translate("HOME", ctx.req)}</a>
-          <a href="/worldMap" class="py-2 hover:bg-gray-700 rounded">{translate("MAP", ctx.req)}</a>
-          <a href="/resume" class="py-2 hover:bg-gray-700 rounded">{translate("RESUME", ctx.req)}</a>
-          <a href="/network" class="py-2 hover:bg-gray-700 rounded">{translate("NETWORK", ctx.req)}</a>
+          <a href="/" class="py-2 hover:bg-gray-700 rounded">
+            {translate("HOME", ctx.req)}
+          </a>
+          <a href="/worldMap" class="py-2 hover:bg-gray-700 rounded">
+            {translate("MAP", ctx.req)}
+          </a>
+          <a
+            href="/resume"
+            class="py-2 hover:bg-gray-700 rounded flex items-center"
+          >
+            {translate("RESUME", ctx.req)}
+            {ctx.user.unspentPoints
+              ? (
+                <img
+                  class="ml-2 h-4 w-4" // Adjust the height and width of the image
+                  src={"/notification.svg"}
+                />
+              )
+              : null}
+          </a>
+          <a href="/network" class="py-2 hover:bg-gray-700 rounded">
+            {translate("NETWORK", ctx.req)}
+          </a>
           {/* <a href="/interview" class="py-2 hover:bg-gray-700 rounded">{translate("TEMP_INTERVIEW", ctx.req)}</a> */}
-          <a href="/api/auth/logout" class="px-4 py-2 border rounded flex gap-4 hover:bg-gray-200 transition-all">
+          <a
+            href="/api/auth/logout"
+            class="px-4 py-2 border rounded flex gap-4 hover:bg-gray-200 transition-all"
+          >
             <GoogleIcon /> {translate("LOGOUT", ctx.req)}
           </a>
         </nav>
--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -3,10 +3,11 @@ import NavLoggedIn from "../islands/NavLoggedIn.tsx";
 import { Head } from "https://deno.land/x/fresh@1.6.3/runtime.ts";
 import { getUser } from "$utils/get_user.ts";
 import { FreshContext } from "$fresh/server.ts";
+import type { Player } from "gameData/playerStats.ts";
 
 
 export default async function Layout(req: Request, ctx: FreshContext) {
-  const user = await getUser(req);
+  const user = await getUser(req) as Player;
   return (
     <>
       <Head>
@@ -14,7 +15,7 @@ export default async function Layout(req: Request, ctx: FreshContext) {
         </meta>
       </Head>
       <div class="flex">
-        {user ? <NavLoggedIn req={req} /> : <Nav req={req}/>}
+        {user ? <NavLoggedIn req={req} user={user}  /> : <Nav req={req}/>}
         <div class="flex-1">
           <ctx.Component />
         </div>
